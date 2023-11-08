080322f09d1b7ea882f4c63c7de7dec30850abb9
breakpack <198799879a@gmail.com>
2023-11-08 13:39:53 +0900

    수정완료

--- cert_tool.py
+++ cert_tool.py
@@ -1,25 +1,24 @@
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5 import uic
 import requests
 from urllib.parse import urlparse, urljoin
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
 
-form_class = uic.loadUiType("cert_tool_ui.ui")[0]
 logfile = open("crawling_log.txt", 'w')
 if len(sys.argv) != 2:
     print('How To Use:\npython cert_tool.py {cheat sheet PATH}')
     sys.exit()
 sheet_path = sys.argv[1]
 Cheat_sheet = open(sheet_path, 'r')
-Xss_q = Cheat_sheet.readlines()
-def crawling(URL, visited, depth=2):
+
+def crawling(url, URL, visited, depth=2):
     if depth == 0 or URL in visited:
         return
     
     visited.add(URL)
-    
+
     try :
         response = requests.get(URL)
         if response.status_code != 200:
@@ -42,12 +41,40 @@ def crawling(URL, visited, depth=2):
             logfile.write(f"[+]current crawling: {link}")
             logfile.write('\n')
             print(f"[+]current crawling: {link}")
-            if urlparse(link).netloc == urlparse(link).netloc:
-                crawling(link, visited, depth-1)
+            if urlparse(link).netloc == urlparse(link).netloc == urlparse(url).netloc:
+                crawling(url, link, visited, depth-1)
     except Exception as e:
         logfile.write(f"[-]Connect Error: {e}")
         logfile.write('\n')
         print(f"[-]Connect Error: {e}")        
+
+def crwaling2(URL, url, visited_links=None):
+    if visited_links is None:
+        visited_links = set()
+    
+    # URL에 접근
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        # HTML 파싱
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # 현재 URL을 방문한 것으로 표시
+        visited_links.add(url)
+
+        # 모든 링크 가져오기
+        links = soup.find_all('a')
+        for link in links:
+            href = link.get('href')
+            absolute_url = urljoin(url, href)
+            # 상대 URL을 절대 URL로 변환
+            
+            # 같은 도메인 내의 URL만 고려 (다른 도메인으로의 이동 방지)
+            if urlparse(absolute_url).netloc == urlparse(url).netloc == urlparse(URL).netloc:
+                if absolute_url not in visited_links:
+                    visited_links.update(crwaling2(URL, absolute_url, visited_links))
+    return visited_links
+
 
 logfile2 = open("check_input_log.txt", 'w')
 def Check_input(link):
@@ -69,7 +96,7 @@ def Check_input(link):
 
 def Is_in_input(URL):
     visited_links = set()
-    crawling(URL, visited_links)
+    crawling(URL, URL, visited_links)
     print(f"[*] In input test start")
     logfile2.write(f"[*] In input test start")
     logfile2.write('\n')
@@ -77,63 +104,105 @@ def Is_in_input(URL):
     logfile2.write(f"In input list : {In_input}")
     return In_input
 
+driver = webdriver.Chrome()
+def Alert_check(URL):
+    print(f'[+]재크롤링 시작')
+    Des_URL_list = crwaling2(URL, URL)
+    for i in Des_URL_list:
+        print(f'- {i}')
+    result = []
+    for URL in list(set(Des_URL_list)):
+        response = requests.get(URL)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        links = soup.find_all('a')
+        absolute_links = set()
+        for link in links:
+            href = link.get('href')
+            absolute_url = urljoin(URL, href)
+            absolute_links.add(absolute_url)
+        for link in absolute_links:
+            print('[+]alert확인 link :', link)
+            try:
+                driver.get(link)
+                try:
+                    alert = driver.switch_to.alert
+                    alert_text = alert.text
+                    print("[++]Alert 텍스트:", alert_text)
+                    # 필요한 작업 수행 (예: 확인 버튼 클릭)
+                    alert.accept()
+                    result.append(link)
+                except:
+                    print("[-]알림이 없거나 처리할 수 없습니다.")
+            except:
+                print('[-]error')         
+    return result
+
+Xss_q = Cheat_sheet.readlines()
 def Attack(URL):
     Des_URL_list = Is_in_input(URL)
-    print("parse OK", Des_URL_list)
+    for i in Des_URL_list:
+        if(i == None):
+            Des_URL_list.remove(i)
+    print("[+]parse OK\n", Des_URL_list)
     for url in Des_URL_list:
+        print(f'[+]form 유무 검사: {url}')
         response = requests.get(url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
             form_tag = soup.find('form')
             if form_tag:
                 action_url = form_tag.get('action')
-                print("Form의 action URL:", action_url)
+                print("[+]Form의 action URL:", action_url)
             else:
-                print("폼이 없거나 action 속성이 없습니다.")
+                print("[-]폼이 없거나 action 속성이 없습니다.")
+                continue
         else:
-            print("페이지에 접근할 수 없습니다. 응답 코드:", response.status_code)
+            print("[-]페이지에 접근할 수 없습니다. 응답 코드:", response.status_code)
+            continue
+        
         input_tags = soup.find_all('input')
         input_names = [input_tag.get('name') for input_tag in input_tags]
         area_tags = soup.find_all('textarea')
-        area_names = [input_tag.get('name') for input_tag in input_tags]
-        print(input_names, area_names)
+        area_names = [area_tag.get('name') for area_tag in area_tags]
+        
+        form_data = []
+        for payload in Xss_q:
+            data = {}
+            for input_name in input_names:
+                data[input_name] = "certtest"
+            for area_name in area_names:
+                data[area_name] = payload
+            form_data.append(data)
+        print('[*]form_data:', form_data)
+        
+        for data_form in form_data:
+            target_url = urljoin(url, action_url)
 
-class WindowClass(QMainWindow, form_class):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.pushButton.clicked.connect(
-            self.functionScan)  # scanbutoon 눌렀을때 연결
+            print("[+]target_url:", target_url)
+            response_req = requests.post(target_url, data=data_form)
+            if response_req.status_code == 200:
+                print('[+]폼 제출성공')
+            else:
+                print('[-]폼 제출실패')
+    print('[*]ALERT가 확인되는 URL: ', Alert_check(URL))
 
-    def functionScan(self):  # scanbutton 눌렀을때 작동
-        logfile.write("gui Start")
-        logfile.write('\n')
-        URL = self.lineEdit.text()
-        URL_OK = 0
-        try:
-            URL_check = requests.get(URL).status_code  # Response 받아옴
-            # 응답이 200이 아닐때
-            self.label_5.setText(
-                "plz check URL") if URL_check != 200 else self.label_5.setText("URL is OK")
-            URL_OK = 1
-        except:
-            # 응답을 받는데 에러가 났다
-            self.label_5.setText("plz check URL")
-        if URL_OK:
-            Attack(URL)
+def functionScan(URL):  # scanbutton 눌렀을때 작동
+    logfile.write("gui Start")
+    logfile.write('\n')
+    URL_OK = 0
+    try:
+        URL_check = requests.get(URL).status_code  # Response 받아옴
+        # 응답이 200이 아닐때
+        print('url is not available') if URL_check != 200 else print("URL is OK")
+        URL_OK = 1
+    except:
+        # 응답을 받는데 에러가 났다
+        print("plz check URL")
+    if URL_OK:
+        Attack(URL)
+        driver.quit()
 
-
-if __name__ == "__main__":
-    # QApplication : 프로그램을 실행시켜주는 클래스
-    app = QApplication(sys.argv)
-
-    # WindowClass의 인스턴스 생성
-    myWindow = WindowClass()
-
-    # 프로그램 화면을 보여주는 코드
-    myWindow.show()
-
-    # 프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
-    app.exec_()
-
+url = input('url?')
+functionScan(url)
 logfile.close()
+logfile2.close()
--- test.py
+++ test.py
@@ -1,72 +1,25 @@
-list = """
-9	17
-1a1b	13.5	25
-aaa7117	9.5	17
-abc1234	10	18
-abcd13	4.5	8
-aim3636	14.5	27
-am1029	4	7
-babo04	17.5	32
-baekho	16	29
-bin1017	19	35
-break03	11	20
-case	5	9
-chiara	13.5	25
-cowe97	17.5	32
-dani22	0	0
-deku77	15	28
-dodo12	13.5	25
-exu37	11	20
-gamja13	15.5	29
-green5	13.5	25
-gyung07	11.5	21
-ham999	16	29
-hanyul04	7	13
-hatemath	6.5	12
-hka	10	18
-hos	12.5	23
-hr0710	11.5	21
-hsk27	13	24
-jjjj53	16	29
-k00098	8	15
-keras21	10.5	19
-lak4021	13	24
-leesan6	10.5	19
-leona16	12.5	23
-lilili	11	20
-lily	14.5	27
-lsp	5	9
-mathking	0	0
-mello47	16	29
-mhd1229	5	9
-mrchar	10.5	19
-mugeun1	7.5	14
-notjsj	15	28
-notporq	17	31
-orange1	10.5	19
-owl321	9	17
-pengpeng	16	29
-ponch12	17	31
-qaeawa	17	31
-qood123	16.5	30
-qwer1234	10.5	19
-song20	8	15
-sp2077	16.5	30
-summer04	18	33
-thisis1	7	13
-wsxuj	0	0
-yamtu1u	8.5	16
-zizon17	7	13
-zz67	12	22
-zzzzz11	0	0
-"""
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+import requests
+from urllib.parse import urlparse, urljoin
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
 
-list = list.split()
-result = 0
-print(list)
-for i in range(len(list)):
-    if i % 3 == 1 :
-        print(list[i])
-        result += float(list[i])
-        
-print('aver: ', result/51)
+Des_URL_list =  ['http://localhost/tem/index.php', 'http://localhost/board/freeboardwrite.php']
+for url in Des_URL_list:
+    
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        form_tag = soup.find('form')
+        if form_tag:
+            action_url = form_tag.get('action')
+            print("[+]Form의 action URL:", action_url)
+        else:
+            print("[-]폼이 없거나 action 속성이 없습니다.")
+    else:
+        print("[-]페이지에 접근할 수 없습니다. 응답 코드:", response.status_code)
+        break     
